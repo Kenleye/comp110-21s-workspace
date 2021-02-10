9adbf4b0cb72e6119abeb7fd6d8ada9358a64138
Kenley Easter <kenleye@live.unc.edu>
2021-02-09 19:28:42 -0500

    ex02 part 1

--- exercises/ex02_p1/fortune_cookie.py
+++ exercises/ex02_p1/fortune_cookie.py
@@ -2,20 +2,34 @@
 
 from random import randint
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730316492"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     print("Your fortune cookie says...")
-    # TODO 2: Print the result of calling your fortune_cookie function.
+    # TODO 2 
+    print(fortune_cookie())
     print("Now, go spread positive vibes!")
 
 
 # TODO 1: Define your fortune_cookie function here.
+def fortune_cookie() -> str: 
+    magic_number: int = randint(1, 11)
+    if magic_number == 2: 
+        return("you will get an A in COMP110")
+    else: 
+        if magic_number == 4:
+            return("you will fail COMP110")
+        else:
+            if magic_number == 6:
+                return("you are amazing and your COMP110 grade doesn't define you")
+            else: 
+                if magic_number == 8:
+                    return("you are so good at COMP110 you will be the next Kris Jordan")
+                else:
+                    return("you should stick to being a psych major")
 
 
-# Python Idiom for "starting" the program when run as a module.
-# The special dunder variable __name__ will be "__main__" when run as module. 
 if __name__ == "__main__":
     main()
--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -1,15 +1,27 @@
 """Tar Heels exercise redux as a structured program."""
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730316492"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
     # TODO 2: Print the response of calling the tar_heels function here.
+    print(tar_heels(choice))
 
 
 # TODO 1: Define the tar_heels function, and its logic, here.
+def tar_heels(number: int) -> str: 
+    if number % 2 == 0 and number % 7 == 00:
+        return("TAR HEELS")
+    else:
+        if number % 2 == 0:
+            return("TAR")
+        else:
+            if number % 7 == 0:
+                return("HEELS")
+            else:
+                return("CAROLINA") 
 
 
 if __name__ == "__main__":
