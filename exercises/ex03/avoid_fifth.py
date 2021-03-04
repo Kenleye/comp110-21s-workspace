6ecad1685647ff384b1a90ae5c984b615be08f56
Kenley Easter <kenleye@live.unc.edu>
2021-03-03 21:01:34 -0500

    final avoid fifth

--- exercises/ex03/avoid_fifth.py
+++ exercises/ex03/avoid_fifth.py
@@ -1,13 +1,24 @@
 """EX03 - avoid_fifth function."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730316492"
 
 
 def main() -> None:
     """Entrypoint of the program."""
-    # Put print statements here to test your function
-    # ex. print(avoid_fifth("hello there"))
+    print(avoid_fifth("hello"))
+    print(avoid_fifth("The sentence we have here possesses E's galore!"))
+ 
 
+def avoid_fifth(x: str) -> str:
+    """No mor3 3s... 3v3r again."""
+    i: int = 0
+    answer: str = ""
+    while i < len(x): 
+        if x[i] != "e" and x[i] != "E":
+            answer = answer + x[i]
+        i += 1
+    return answer
+    
 
 if __name__ == "__main__":
     main()
